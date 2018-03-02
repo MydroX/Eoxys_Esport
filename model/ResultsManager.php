fd9435cbabd709ae52a0125151dc611e54066e18
MydroX <maxime@zinutti.fr>
2018-03-02 03:10:04 +0100

    Modification nom de la class getResults en getResultsHome

--- model/ResultsManager.php
+++ model/ResultsManager.php
@@ -5,7 +5,7 @@ namespace Eoxys_Esport\model\;
 require_once('model/Manager.php');
 
 class ResultsManager extends Manager {
-    public function getResults() {
+    public function getResultsHome() {
         $db = $this->dbConnect();
         $req = $db->query('SELECT * FROM results ORDER BY id DESC LIMIT 0, 3');
 
