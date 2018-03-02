03874647a6bc2757c9735fed58499248a70ef677
MydroX <maxime@zinutti.fr>
2018-03-02 02:59:42 +0100

    Modif nom class getResults en getResultsHome

---
+++ controller/frontend.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once ('model/NewsManager.php');
+require_once ('model/ResultsManager.php');
+
+function home() {
+    $newsManager = new \Eoxys_Esport\model\getNewsHome();
+    $resultsManager = new \Eoxys_Esport\model\getResultsHome();
+
+}
