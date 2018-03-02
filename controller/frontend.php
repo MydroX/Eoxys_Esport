929e20e493a77fb12ff478e9ab84c649b3ce3ee2
MydroX <maxime@zinutti.fr>
2018-03-02 03:55:27 +0100

    Modif creation objet dans les variable (Mauvais appels)

--- controller/frontend.php
+++ controller/frontend.php
@@ -4,8 +4,8 @@ require_once ('model/NewsManager.php');
 require_once ('model/ResultsManager.php');
 
 function home() {
-    $newsManager = new \Eoxys_Esport\model\getNewsHome();
-    $resultsManager = new \Eoxys_Esport\model\getResultsHome();
+    $newsManager = new \Eoxys_Esport\model\NewsManager();
+    $resultsManager = new \Eoxys_Esport\model\ResultsManager();
 
     require ('view/frontend/home.php');
 }
