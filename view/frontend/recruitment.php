ce7a14de2765e41fcabdcbd34434a727ae0e4d08
MydroX <maxime@zinutti.fr>
2018-03-05 15:39:18 +0100

    Création page recrutement

--- view/frontend/recruitment.php
+++ view/frontend/recruitment.php
@@ -1,8 +1,11 @@
 <?php $title = 'Eoxys Esport - Recrutement'; ?>
+<?php $css = 'recruitment.css'; ?>
 
 <?php ob_start(); ?>
 
-<h1>En construction</h1>
+<section id="form">
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe5AzyuBaG2asP8uz3YZJeJ7otHEN_ffQje2hk5oviHawkp2A/viewform?embedded=true" width="760" height="850" frameborder="0" marginheight="0" marginwidth="0">Chargement en cours...</iframe>
+</section>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
