26d3f4c3c6fee53c12349e134966c5e13cc8e38b
MydroX <maxime@zinutti.fr>
2018-03-09 04:01:49 +0100

    Deplacement du boutton 'Menu' + Création formulaire contact

--- view/frontend/contact.php
+++ view/frontend/contact.php
@@ -1,8 +1,22 @@
 <?php $title = 'Eoxys Esport - Contact'; ?>
+<?php $css = 'contact.css'; ?>
 
 <?php ob_start(); ?>
 
-<h1>En construction</h1>
+<section class="wrapper">
+    <div class="left">
+        <h1>Contactez nous</h1>
+        <form method="post" action="?here=email-reception">
+            <input type="email" name="email" placeholder="Email" required/><br />
+            <input type="text" name="object" placeholder="Objet" required/><br />
+            <textarea placeholder="Message" name="message" cols="60" rows="10" required/></textarea>
+        </form>
+    </div>
+    <div class="right">
+        <h1>Informations</h1>
+        <p>Mail : eoxys.esport@gmail.com</p>
+    </div>
+</section>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
